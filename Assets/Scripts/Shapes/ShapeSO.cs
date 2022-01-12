06acff91fa8456dfdad9d06572fcdda9299e8899
Torin Porter <torin.m.porter@gmail.com>
2022-01-12 22:51:47 +0000

    Removed pointless enums

--- Assets/Scripts/Shapes/ShapeSO.cs
+++ Assets/Scripts/Shapes/ShapeSO.cs
@@ -5,40 +5,15 @@ using UnityEngine.Audio;
 
 namespace Frontfire.Shapeshifter2
 {
-    public enum ShapeMovementType
-    {
-        Free,
-        FreeFast,
-        FixedPosition,
-        Vertical,
-        Horizontal,
-        FixedDirection,
-    }
-
-    public enum ShapeShootType
-    {
-        None,
-        Free,
-        Vertical,
-        Horizontal,
-        FixedDirection,
-        AllDirections,
-        Cycle
-    }
-
     public abstract class ShapeSO : ScriptableObject
     {
         [SerializeField] private Sprite shapeSprite;
         [SerializeField] private Color shapeColor;
         [SerializeField] private AudioMixerSnapshot shapeAudioSnapshot;
-        [SerializeField] private ShapeMovementType shapeMovementType;
-        [SerializeField] private ShapeShootType shapeShootType;
 
-        public Sprite Sprite { get { return shapeSprite; } }
-        public Color Color { get { return shapeColor; } }
-        public AudioMixerSnapshot AudioMixerSnapshot { get { return shapeAudioSnapshot; } }
-        public ShapeMovementType ShapeMovementType { get { return shapeMovementType; } }
-        public ShapeShootType ShapeShootType { get { return shapeShootType; } }
+        public Sprite Sprite => shapeSprite;
+        public Color Color => shapeColor;
+        public AudioMixerSnapshot AudioMixerSnapshot => shapeAudioSnapshot;
 
         public abstract Vector2 Move(Vector2 position, Vector2 direction);
         public abstract Vector2 Shoot(Vector2 position, Vector2 direction);
